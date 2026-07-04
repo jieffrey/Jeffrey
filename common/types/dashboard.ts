659ae07cb0dc3047977f232b6e7552b48f2cbbf4
jieffrey <kyooon08@gmail.com>
2026-07-04 18:50:39 +0700

    dashboard-typess

---
+++ common/types/dashboard.ts
@@ -0,0 +1,35 @@
+import { ReactNode } from "react";
+
+export interface DashboardWidget {
+  id: string;
+  title: string;
+  subtitle?: string;
+  icon?: ReactNode;
+}
+
+export interface StatCard {
+  title: string;
+  value: string | number;
+  change?: string;
+}
+
+export interface Repository {
+  name: string;
+  description: string;
+  language: string;
+  stars: number;
+}
+
+export interface SpotifyTrack {
+  title: string;
+  artist: string;
+  album: string;
+  playing: boolean;
+}
+
+export interface GithubStats {
+  followers: number;
+  following: number;
+  repositories: number;
+  commits: number;
+}
